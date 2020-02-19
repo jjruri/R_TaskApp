40d0bcacf31ea2d51592f0a751062c471319309b
jjruri <jj.ruri11280506@gmail.com>
2020-02-19 11:22:10 +0900

    Initial Commit

---
+++ R_TaskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  R_TaskApp
+//
+//  Created by 佐藤るり on 2020/02/19.
+//  Copyright © 2020 junya.satou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
