570698564673d4dbcfa760ba150a1c1e88f438e3
jjruri <jj.ruri11280506@gmail.com>
2020-02-21 16:42:56 +0900

    デリゲートの記述を追記。プロトコルとメソッドを記載してエラーを解消。

--- R_TaskApp/ViewController.swift
+++ R_TaskApp/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //tableViewの表示・挙動をTableview自身で解決せずにViewControllerに任せるという処理を記載
+            tableView.delegate = self
+            tableView.dataSource = self
+    }
+    
+    // データの数（＝セルの数）を返すメソッド ★必須
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    // 各セルの内容を返すメソッド　★必須
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+
+    // 各セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //★現時点では空にしておく
+    }
+
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
+        return.delete
+    }
+
+    // Delete ボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        //★現時点では空にしておく
     }
 
 
