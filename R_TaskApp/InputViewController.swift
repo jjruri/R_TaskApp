903bed30d9b1198191e401c4a74b92b083486044
jjruri <jj.ruri11280506@gmail.com>
2020-03-01 11:14:57 +0900

    画面遷移を追加

---
+++ R_TaskApp/InputViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InputViewController.swift
+//  R_TaskApp
+//
+//  Created by 佐藤るり on 2020/03/01.
+//  Copyright © 2020 junya.satou. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
