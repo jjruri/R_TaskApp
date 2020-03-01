ba8ea256103f41dfcb157e5a9d933d442574009a
jjruri <jj.ruri11280506@gmail.com>
2020-03-01 11:24:33 +0900

    タスク記入画面のUIを設定

--- R_TaskApp/InputViewController.swift
+++ R_TaskApp/InputViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
